880f2a23c6c97c9bdd52fd6de70ff9efa004ba58
Project-x109 <amanuelgirma109@gmail.com>
2023-10-05 00:01:25 +0300

    minors

---
+++ Admin/get_user_data.php
@@ -0,0 +1,18 @@
+<?php
+include "../connect.php";
+session_start();
+if (isset($_GET['id'])) {
+    $userId = $_GET['id'];
+    $sql = "SELECT * FROM users WHERE id = $userId";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $userData = $result->fetch_assoc();
+        echo json_encode($userData);
+    } else {
+        echo json_encode(['error' => 'User not found']);
+    }
+} else {
+    echo json_encode(['error' => 'Invalid request']);
+}
+$conn->close();
